6df4f3bed36f1e37a79ac0053fae7cc211aaf53c
Surajk37 <surajkhot9993@gmail.com>
2023-01-02 21:53:43 +0530

    [Suraj] Welcome Message

---
+++ Generics236/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("\t\t\t----------------------------\n\t\t\tWelcome To Generics Problems\n\t\t\t----------------------------\n");
+            Console.ReadLine();
+        }
+    }
+}
