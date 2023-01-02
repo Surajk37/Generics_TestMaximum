f5bfefacebe2f68bbbd3db055d512c43360fea32
Surajk37 <surajkhot9993@gmail.com>
2023-01-02 22:11:36 +0530

    [Suraj] Checked Maximum Interger Number

---
+++ Generics236/FindMaxNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics236
+{
+    class FindMaxNumber
+    {
+        public static void MaxInt(int a,int b,int c)
+        {
+            if ((a.CompareTo(b) > 0) && (a.CompareTo(c) > 0))
+            {
+                Console.WriteLine("\nThe maximum Integer number is:" + a);
+            }
+            else if ((b.CompareTo(a) > 0) && (b.CompareTo(c) > 0))
+            {
+                Console.WriteLine("\nThe maximum Integer number is: " + b);
+            }
+            else
+            {
+                Console.WriteLine("\nThe maximum Integer number is : " + c);
+            }
+        }
+    }
+}
--- Generics236/Program.cs
+++ Generics236/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Generics
+﻿using Generics236;
+
+namespace Generics
 {
     class Program
     {
         public static void Main()
         {
             Console.WriteLine("\t\t\t----------------------------\n\t\t\tWelcome To Generics Problems\n\t\t\t----------------------------\n");
+            Console.WriteLine("\n\tPlease enter an option");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1:Find integer maximum number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter Three Values");
+
+            switch (option)             //Calling different methods using switch case
+            {
+                case 1:
+                    int a,b, c;
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("a = {0}\t b = {1}\t c = {2} ",a,b,c);
+                    FindMaxNumber.MaxInt(a,b,c);
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
